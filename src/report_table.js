7e28c9be453b7573b4371356b9599330df8e9369
krausediego <diego.krause@ideamaker.com.br>
2025-04-01 09:31:46 -0300

    feat: Change opacity heatmap.

--- src/report_table.js
+++ src/report_table.js
@@ -119,7 +119,7 @@ const buildReportTable = function (
     const colorScale = d3
       .scaleSequential()
       .domain([min, max])
-      .interpolator(d3.interpolateRgb(colorStart + '80', colorEnd + '80'));
+      .interpolator(d3.interpolateRgb(colorStart + '90', colorEnd + '90'));
     const getTextWidth = function (text, font = '') {
       // re-use canvas object for better performance
       var canvas =
